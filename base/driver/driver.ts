c65a5cb6c6ea71d3f922077554412b1e49094efc
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-05-03 14:38:19 +0300

    Added Traces to HTML report. Fixed close() method

--- base/driver/driver.ts
+++ base/driver/driver.ts
@@ -8,14 +8,12 @@ export default class Driver {
 	async start() {
 		this.browser = await chromium.launch();
 		this.context = await this.browser.newContext();
-		// this.context.tracing.start({screenshots: true, snapshots: true});
 		this.page = await this.context.newPage();
 	}
 
 	async close() {
-		// this.context.tracing.stop({path: 'trace.zip'});
-		this.page.close();
-		this.context.close();
+		await this.page.close();
+		await this.context.close();
 	}
 }
 
